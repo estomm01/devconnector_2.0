45a559f3d8a755ede2a630b38d6b18b067cef356
Markus Maasikas <markus@hotmail.com>
2019-09-04 14:24:34 -0400

    updated user.js

---
+++ routes/api/users.js
@@ -0,0 +1,32 @@
+const express = require(`express`);
+const router = express.Router();
+const { check, validationResult } = require(`express-validator/check`);
+
+//route POST api/posts
+//desc Register User
+//access Public
+
+router.post(
+  `/`,
+  [
+    check(`name`, `Name is required`)
+      .not()
+      .isEmpty(),
+    check(`email`, `Please include a valid email`).isEmail(),
+    check(
+      `passowrd`,
+      `Please enter a password with 6 or more characters`
+    ).isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send(`Users route`);
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const app = express();
 //connect Database
 connectDB();
 
-// const mongoose = require("mongoose");
+// Init Middleware
+
+app.use(express.json({ extended: false }));
 
 app.get(`/`, (req, res) => res.send(`API Running`));
+
+//Define Routes
+app.use(`/api/users`, require(`./routes/api/users`));
+app.use(`/api/auth`, require(`./routes/api/auth`));
+app.use(`/api/profile`, require(`./routes/api/profile`));
+app.use(`/api/posts`, require(`./routes/api/posts`));
+
 // const routes = require("./routes");
 const PORT = process.env.PORT || 3000;
 
